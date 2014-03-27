52b5a4a7d526211b63a2a4c4b74d23acd027b0b0
Dmitriy Konovalov <d.konovalov.39@gmail.com>
2014-03-27 17:18:23 +0200

    made little fixes in user.rb, task.rb and config/application.rb

--- app/models/task.rb
+++ app/models/task.rb
@@ -1,6 +1,6 @@
 class Task < ActiveRecord::Base
 
-  attr_accessible :user_id, :name, :creator, :project_id
+
 
   belongs_to :user
   belongs_to :project
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,6 @@
 class User < ActiveRecord::Base
 
-  attr_accessible :name, :login, :password
+
 
   has_and_belongs_to_many :projects
   has_many                :tasks
